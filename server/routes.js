10d572ab36fd9d1046257cc666aad6733137dba0
Vidhya G <vidhya.g@Bridgeglobal.local>
2018-01-03 17:07:18 +0530

    switched to mysql2

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const mysql = require('mysql');
+const mysql = require('mysql2');
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('flowproj', 'flowproj', 'bridgeglobal', {
